1fe10772b05bb0030c620d7ffb3b64b8d4d8389c
R4NC0X CR4K3R <stevenguzman.cars@gmail.com>
2025-11-26 22:23:39 -0500

    COMMIT 01

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// JavaScript Document
+
+$(document).ready(function(){
+'use strict';
+$('body').hide();0
+$('body').fadeIn(1000);
+});
+
+//SCREENSAVER ---------------------------------------------------
+
+
+$( document ).ready(function( $ ) {
+		'use strict';
+		$( '#screensaver' ).sliderPro({
+			width: 1080,
+			height: 1920,
+			slideDistance: 0,
+			arrows: false,
+			buttons: false,
+			waitForLayers: true,
+			thumbnailWidth: 200,
+			thumbnailHeight: 100,
+			thumbnailPointer: true,
+			autoplay: true,
+			autoScaleLayers: true,
+			breakpoints: {
+				500: {
+					thumbnailWidth: 120,
+					thumbnailHeight: 50
+				}
+			}
+		});
+	});
+//SCREENSAVER END ----------------------------------------------
+
+//Slider pro ---------------------------------------------------
+$( document ).ready(function( $ ) {
+		'use strict';
+		$( '#my-slider' ).sliderPro({
+			width: 1080,
+			height: 720,
+			slideDistance: 0,
+			arrows: true,
+			buttons: false,
+			waitForLayers: true,
+			thumbnailWidth: 200,
+			thumbnailHeight: 100,
+			thumbnailPointer: true,
+			autoplay: true,
+			autoScaleLayers: true,
+			breakpoints: {
+				500: {
+					thumbnailWidth: 120,
+					thumbnailHeight: 50
+				}
+			}
+		});
+	});
+//Slider pro end ----------------------------------------------
+
+
+
+//Videos ---------------------------------------------------
+
+//Videos ---------------------------------------------------
+
+
+
+
+
